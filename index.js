defa85da8be2771a3a32f62b3f5aa5e783c2cdf4
AdityaSharda1 <adityasharda541@gmail.com>
2024-07-11 00:24:04 +0530

    new navbar

---
+++ index.js
@@ -0,0 +1,9 @@
+// function toggleMobileMenu(menu) {
+//     menu.classList.toggle('open');
+// }
+
+function toggleMobileMenu(icon) {
+    icon.classList.toggle("open");
+    document.querySelector(".custom-mobile-menu").classList.toggle("open");
+  }
+  
