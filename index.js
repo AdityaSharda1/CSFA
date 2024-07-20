ee39ebe99b7d953cc0ebe7067c09d060cbe29aeb
AdityaSharda1 <adityasharda541@gmail.com>
2024-07-20 20:02:55 +0530

    little updates

--- index.js
+++ index.js
@@ -7,3 +7,24 @@ function toggleMobileMenu(icon) {
     document.querySelector(".custom-mobile-menu").classList.toggle("open");
   }
   
+
+  // testimonials
+
+  var swiper = new Swiper(".c-testimonials", {
+    spaceBetween: 30,
+    effect: "fade",
+    loop: true,
+    mousewheel: {
+      invert: false
+    },
+    // autoHeight: true,
+    pagination: {
+      el: ".c-testimonials__pagination",
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".c-testimonials__arrow-next",
+      prevEl: ".c-testimonials__arrow-prev"
+    }
+  });
+  
